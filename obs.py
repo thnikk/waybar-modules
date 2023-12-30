117fd70a23a481e1af13e14b4d0c4b26ca3e42b2
thnikk <thnikk645@gmail.com>
2023-12-29 21:26:58 -0800

    Fixed linter warnings

--- obs.py
+++ obs.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-# Shows the current OBS scene and changes color if recording.
-# Use with exec-if: pgrep OBS
-# Uses obsws-python: https://github.com/aatikturk/obsws-python
-import obsws_python as obs
+"""
+Shows the current OBS scene and changes color if recording.
+Use with exec-if: pgrep OBS
+Uses obsws-python: https://github.com/aatikturk/obsws-python
+"""
 import json
+import sys
+import obsws_python as obs
 
 output = {}
-icon = ""
+ICON = ""
 
 try:
     cl = obs.ReqClient(host='localhost', port=4455, password='password')
 except ConnectionRefusedError:
-    output["text"] = "<span color=\"#d8dee977\">" + icon + "</span>"
+    output["text"] = "<span color=\"#d8dee977\">" + ICON + "</span>"
     print(json.dumps(output))
-    quit()
+    sys.exit(0)
 
 status = cl.get_record_status()
 scene = cl.get_current_program_scene().current_program_scene_name
 
-output["text"] = "{} {}".format(icon, scene)
+output["text"] = f"{ICON} {scene}"
 if status.output_duration > 0:
-    output["text"] = "<span color=\"#bf616a\">{}</span> {}".format(
-            icon, str(int(status.output_duration/1000)))
+    output["text"] = (f"<span color=\"#bf616a\">{ICON}</span> "
+                      f"{int(status.output_duration/1000)}")
 print(json.dumps(output))
