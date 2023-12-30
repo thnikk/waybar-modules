a1d1a69e8af9d013b7e161b5c45ed9cb64ecc360
thnikk <thnikk645@gmail.com>
2023-12-29 21:22:49 -0800

    Fixed linter warnings

--- ups.py
+++ ups.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+"""
+Module for cyberpower UPS to show estimated power usage of system.
+Author: thnikk
+"""
 import subprocess
 import json
+import argparse
+
+parser = argparse.ArgumentParser(description="")
+parser.add_argument('name', action='store',
+                    type=int, help='Part of device name to search for')
+args = parser.parse_args()
 
 output = subprocess.run(
         ["pwrstat", "-status"],
@@ -16,10 +26,10 @@ for line in output:
         value = line.split("..")[-1].strip(".").strip()
         stats[key] = value
 
-load = str(int(stats["load"].split(" ")[0]) - 126)
+LOAD = str(int(stats["load"].split(" ")[0]) - 126)
 tooltip = "<span color='#8fa1be' font_size='16pt'>UPS stats</span>\n" + \
         "Remaining runtime: " + stats["remaining-runtime"] + "\n" + \
         "Load: " + stats["load"] + "\n" + \
         "Battery Capacity: " + stats["battery-capacity"]
 
-print(json.dumps({"text": load, "tooltip": tooltip}))
+print(json.dumps({"text": LOAD, "tooltip": tooltip}))
