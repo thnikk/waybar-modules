9e243d12ae4f55a80ca38d329ea2ee7d77e0826c
thnikk <thnikk645@gmail.com>
2023-12-30 02:54:39 -0800

    Fixed linter warnings

--- battery.py
+++ battery.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-# Module for getting combined battery percentage for a supplied argument.
-# For example, running the script with the argument BAT would combine
-# BAT0 and BAT1 if you're using a laptop with multiple batteries.
+"""
+Module for getting combined battery percentage for a supplied argument.
+For example, running the script with the argument BAT would combine
+BAT0 and BAT1 if you're using a laptop with multiple batteries.
+Author: thnikk
+"""
 import os
 import json
 import argparse
 
 # Path to all battery devices
-device_path = "/sys/class/power_supply"
+DEV_PATH = "/sys/class/power_supply"
 
 # Parse arguments for device
 parser = argparse.ArgumentParser(description="Combined battery module")
@@ -16,49 +19,41 @@ parser.add_argument('name', action='store',
 args = parser.parse_args()
 
 # Combined levels and capacities
-all_full = 0
-all_now = 0
+ALL_FULL = 0
+ALL_NOW = 0
 
 # Output tooltip
-tooltip = ""
+TOOLTIP = ""
 
 
 # Function to get percentage from level and capacity values
 def get_percentage(now, full):
+    """ Calculate percentage from current and max values """
     return int((now / full) * 100)
 
 
 # Iterate through all power supply devices
-for device in os.listdir(device_path):
+for device in os.listdir(DEV_PATH):
     # If the device is supplied as an argument
     if args.name[0] in device:
         # Get values
         device_full = int(open(
-            device_path + "/" + device + "/energy_full").read())
+            DEV_PATH + "/" + device + "/energy_full", encoding='utf-8').read())
         device_now = int(open(
-            device_path + "/" + device + "/energy_now").read())
+            DEV_PATH + "/" + device + "/energy_now", encoding='utf-8').read())
         # Add values to max
-        all_full += device_full
-        all_now += device_now
+        ALL_FULL += device_full
+        ALL_NOW += device_now
         # Get devices percentage
         device_percent = get_percentage(device_now, device_full)
         # Append individual battery percentage
-        tooltip += device + ": " + str(device_percent) + "\n"
+        TOOLTIP += device + ": " + str(device_percent) + "\n"
 
 # Get combined battery percentage
-all_percent = get_percentage(all_now, all_full)
-# Set an icon for the combined percentage
-if all_percent > 80:
-    icon = " "
-elif all_percent > 60:
-    icon = " "
-elif all_percent > 40:
-    icon = " "
-elif all_percent > 20:
-    icon = " "
-else:
-    icon = " "
+all_percent = get_percentage(ALL_NOW, ALL_FULL)
+icons = [" ", " ", " ", " ", " "]
+icon = icons[all_percent // 25]
 
 # Print json output
 print(json.dumps({"text": icon + str(all_percent) + "%",
-                  "tooltip": tooltip.rstrip()}))
+                  "tooltip": TOOLTIP.rstrip()}))
