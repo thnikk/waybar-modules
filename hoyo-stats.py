1bd972ddb210fc33b6b1873bf5dcba8ffc24efd6
thnikk <thnikkshop@gmail.com>
2024-03-30 23:21:19 -0700

    Refactored and added separate modes for games

--- hoyo-stats.py
+++ hoyo-stats.py
@@ -7,20 +7,34 @@ from datetime import datetime, timezone
 import os
 import time
 import configparser
+import argparse
 import requests
 import genshin
 
 cache_file = os.path.expanduser("~/.cache/hoyo-stats.json")
 config_file = os.path.expanduser("~/.config/hoyo-stats.ini")
 
-# Create cache file if it doesn't exist
-if not os.path.exists(config_file):
-    with open(config_file, "a", encoding='utf-8') as f:
-        f.write("[settings]\nltuid = \nltoken = \ngenshin_id = \nhsr_id =")
-        f.close()
-# Load config from cache
-config = configparser.ConfigParser()
-config.read(config_file)
+
+def get_args():
+    """ Get arguments """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-g', '--game', default='genshin', help='Game to get info for')
+    args = parser.parse_args()
+    return args
+
+
+def get_config():
+    """ Get or create config file """
+    # Create config file if it doesn't exist
+    if not os.path.exists(config_file):
+        with open(config_file, "a", encoding='utf-8') as f:
+            f.write("[settings]\nltuid = \nltoken = \ngenshin_id = ")
+            f.close()
+    # Load config from cache
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    return config
 
 
 def wait_network() -> None:
@@ -46,7 +60,15 @@ def time_diff(now, future, rate):
     return f"{until_next // 60}:{until_next % 60}"
 
 
-async def main():
+def old(old, now, min_diff):
+    """ Check age of datetime objects """
+    diff = (datetime.now() - old).total_seconds()
+    if diff > 60*min_diff:
+        return True
+    return False
+
+
+async def generate_cache(config, game):
     """ Main function """
     wait_network()
     cookies = {
@@ -56,11 +78,17 @@ async def main():
     # Get current time
     time_now = datetime.now(timezone.utc)
 
-    # Main output dictionary
-    main_dict = {}
+    with open(cache_file, encoding='utf-8') as json_file:
+        cache = json.load(json_file)
 
-    try:
-        if config["settings"]["genshin_id"]:
+    if game == 'genshin':
+        try:
+            if old(
+                datetime.fromtimestamp(cache['Genshin']['timestamp']),
+                time_now, 5
+            ):
+                raise ValueError
+        except (KeyError, ValueError):
             # Get genshin info
             genshin_client = genshin.Client(cookies)
             genshin_notes = await genshin_client.get_notes()
@@ -68,22 +96,29 @@ async def main():
                     config["settings"]["genshin_id"])
             com_prog = genshin_notes.completed_commissions + \
                 int(genshin_notes.claimed_commission_reward)
-            main_dict["Genshin"] = {
+            cache['Genshin'] = {
                 "Resin": genshin_notes.current_resin,
                 "Until next 40": time_diff(
                     time_now, genshin_notes.resin_recovery_time, 8),
                 "Dailies completed": f"{com_prog}/5",
-                "Remaining boss discounts": genshin_notes.remaining_resin_discounts,
+                "Remaining boss discounts":
+                genshin_notes.remaining_resin_discounts,
                 "Realm currency": genshin_notes.current_realm_currency,
-                "Abyss progress": (f"{genshin_user.abyss.current.max_floor} "
-                                   f"{genshin_user.abyss.current.total_stars}"
-                                   )
+                "Abyss progress": (
+                    f"{genshin_user.abyss.current.max_floor} "
+                    f"{genshin_user.abyss.current.total_stars}"
+                    ),
+                "timestamp": datetime.timestamp(time_now)
             }
-    except KeyError:
-        pass
 
-    try:
-        if config["settings"]["hsr_id"]:
+    if game == 'hsr':
+        try:
+            if old(
+                datetime.fromtimestamp(cache['HSR']['timestamp']),
+                time_now, 5
+            ):
+                raise ValueError
+        except (KeyError, ValueError):
             # Get HSR info
             hsr_client = genshin.Client(cookies, game=genshin.Game.STARRAIL)
             hsr_notes = await hsr_client.get_starrail_notes()
@@ -91,83 +126,103 @@ async def main():
                     config["settings"]["hsr_id"])
             moc_floors = [floor.name for floor in hsr_user.floors]
             daily_prog = hsr_notes.current_train_score // 100
-            main_dict["HSR"] = {
+            cache["HSR"] = {
                 "Trailblaze power": hsr_notes.current_stamina,
-                "Until next 40": time_diff(
-                    time_now, hsr_notes.stamina_recovery_time, 6),
+                "Until next 40":
+                time_diff(time_now, hsr_notes.stamina_recovery_time, 6),
                 "Dailies completed": f"{daily_prog}/5",
-                "Remaining boss discounts": hsr_notes.remaining_weekly_discounts,
-                "SU weekly score": "{}/{}".format(hsr_notes.current_rogue_score,
-                                                  hsr_notes.max_rogue_score),
-                "MoC progress": f"{len(moc_floors)} {hsr_user.total_stars}"
+                "Remaining boss discounts":
+                hsr_notes.remaining_weekly_discounts,
+                "SU weekly score":
+                f"{hsr_notes.current_rogue_score}/"
+                f"{hsr_notes.max_rogue_score}",
+                "MoC progress": f"{len(moc_floors)} {hsr_user.total_stars}",
+                "timestamp": datetime.timestamp(time_now)
             }
-    except KeyError:
-        pass
 
     # Write dictionary to json file
-    with open(cache_file, "w", encoding='utf-8') as save_file:
-        save_file.write(json.dumps(main_dict, indent=4))
+    with open(cache_file, "w", encoding='utf-8') as file:
+        file.write(json.dumps(cache, indent=4))
 
-# Check modification date
-try:
-    mod_time = datetime.fromtimestamp(os.path.getmtime(cache_file))
-    mod_seconds = (datetime.now() - mod_time).total_seconds()
-    if mod_seconds > 60*2:
+    return cache
+
+
+def get_cache(config, game):
+    """ Generate cache if necessary """
+    try:
+        mod_time = datetime.fromtimestamp(os.path.getmtime(cache_file))
+        mod_seconds = (datetime.now() - mod_time).total_seconds()
+        if mod_seconds > 60*2:
+            raise ValueError
+    except (FileNotFoundError, ValueError):
         try:
-            asyncio.run(main())
+            asyncio.run(generate_cache(config, game))
         except genshin.errors.GenshinException:
             time.sleep(5)
-except FileNotFoundError:
-    try:
-        asyncio.run(main())
-    except genshin.errors.GenshinException:
-        time.sleep(5)
+    with open(cache_file, encoding='utf-8') as json_file:
+        return json.load(json_file)
 
 
-# Load cache from file
-cache = {}
-with open(cache_file, encoding='utf-8') as json_file:
-    cache = json.load(json_file)
+def genshin_module(cache):
+    """ Generate waybar output for Genshin """
+    output = {}
+    dailies = cache["Dailies completed"].split("/")
+    dailies_completed = int(dailies[0]) == int(dailies[1])
+    realm_currency_capped = cache['Realm currency'] >= 2000
 
-text = []
-tooltip = []
+    if not dailies_completed and not realm_currency_capped:
+        output['class'] = 'red'
+    if dailies_completed and realm_currency_capped:
+        output['class'] = 'yellow'
+    if not dailies_completed and realm_currency_capped:
+        output['class'] = 'orange'
 
-if not cache:
-    config_fail()
-    sys.exit(0)
+    output['text'] = f" {cache['Resin']}"
 
-for game, data in cache.items():
+    cache.pop('timestamp')
+    output['tooltip'] = (
+        "<span color='#8fa1be' font_size='16pt'>"
+        "Genshin Stats</span>\n") + '\n'.join(
+            f'{key}: {value}' for key, value in cache.items()
+    )
+    return output
 
-    if game == "Genshin":
-        dailies = int(data["Dailies completed"].split("/")[0])
-        # Set color based on status
-        if dailies < 5 and data["Realm currency"] >= 2000:
-            ICON = "<span color=\"#d08770\"> </span>"
-        elif data["Realm currency"] >= 2000:
-            ICON = "<span color=\"#ebcb8b\"> </span>"
-        elif dailies < 5:
-            ICON = "<span color=\"#bf616a\"> </span>"
-        else:
-            ICON = "  "
-        text.append(ICON + str(data["Resin"]))
-    elif game == "HSR":
-        su_points = int(data["SU weekly score"].split("/")[0])
-        su_max = int(data["SU weekly score"].split("/")[1])
-        dailies = int(data["Dailies completed"].split("/")[0])
-        if dailies < 5:
-            ICON = "<span color=\"#bf616a\"> </span>"
-        elif su_points < su_max and datetime.today().weekday() >= 5:
-            ICON = "<span color=\"#ebcb8b\"> </span>"
-        else:
-            ICON = " "
-        text.append(ICON + str(data["Trailblaze power"]))
 
-    # Create tooltip
-    tooltip.append(
-            ("<span color='#8fa1be' font_size='16pt'>"
-             f"{game} Stats</span>\n") +
-            '\n'.join(
-                f'{key}: {value}' for key, value in data.items()))
+def hsr_module(cache):
+    """ Generate waybar output for HSR """
+    output = {}
+    dailies = cache["Dailies completed"].split("/")
+    dailies_completed = int(dailies[0]) == int(dailies[1])
 
-# Print output as json
-print(json.dumps({"text": "  ".join(text), "tooltip": "\n\n".join(tooltip)}))
+    if not dailies_completed:
+        output['class'] = 'red'
+
+    output['text'] = f" {cache['Trailblaze power']}"
+
+    cache.pop('timestamp')
+    output['tooltip'] = (
+        "<span color='#8fa1be' font_size='16pt'>"
+        "HSR Stats</span>\n") + '\n'.join(
+            f'{key}: {value}' for key, value in cache.items()
+    )
+    return output
+
+
+def main():
+    """ Main function """
+    args = get_args()
+    config = get_config()
+    cache = asyncio.run(generate_cache(config, args.game))
+
+    if args.game == "genshin":
+        output = genshin_module(cache['Genshin'])
+    elif args.game == "hsr":
+        output = hsr_module(cache['HSR'])
+    else:
+        output = {"text": "Invalid game specified"}
+
+    print(json.dumps(output))
+
+
+if __name__ == "__main__":
+    main()
