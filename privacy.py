5c1e62ec0bc57a2c5edfc64a22947577d36392c1
thnikk <thnikkshop@gmail.com>
2024-04-02 14:35:26 -0700

    Added webcam detection

--- privacy.py
+++ privacy.py
@@ -3,8 +3,35 @@
 Description: Privacy module that doesn't crash waybar.
 Author: thnikk
 """
-from subprocess import run
+from subprocess import run, CalledProcessError
 import json
+from glob import glob
+
+
+def process_name(pid):
+    """ Get process name for PID """
+    with open(f'/proc/{pid}/cmdline', 'r', encoding='utf-8') as file:
+        return file.read().split('\x00')[0].split()[0].split('/')[-1]
+
+
+def get_webcams():
+    """ Get processes using video devices """
+    webcams = {}
+    for device in glob("/dev/video*"):
+        try:
+            output = run(
+                ['fuser', device], check=True, capture_output=True
+            ).stdout.decode('utf-8').strip().split()
+            names = []
+            for pid in output:
+                name = process_name(pid)
+                if 'wireplumber' not in name:
+                    names.append(name)
+            if names:
+                webcams[device] = names
+        except CalledProcessError:
+            pass
+    return webcams
 
 
 def json_output(command):
@@ -26,7 +53,7 @@ def get_prop(full_props, prop_list):
 
 
 def get_categories(pw):
-    """ Get dictionary of running strings by programs """
+    """ Get dictionary of running category by programs """
     running = {}
     for item in pw:
         try:
@@ -46,7 +73,21 @@ def get_categories(pw):
                     running[mtype].append(program)
         except KeyError:
             pass
+    webcams = get_webcams()
+    running.update(webcams)
     return running
+
+
+def get_icons(categories):
+    """ Get string of icons for active categories """
+    icon_lookup = {'Audio': '', 'Video': '', 'Webcam': ''}
+    icons = []
+    for category in categories:
+        if '/dev/video' in category:
+            category = 'Webcam'
+        if icon_lookup[category] not in icons:
+            icons.append(icon_lookup[category])
+    return ' '.join(icons)
 
 
 def main():
@@ -56,17 +97,15 @@ def main():
 
     output = {'class': 'green'}
 
-    icon_lookup = {'Audio': '', 'Video': ''}
-    icon = " ".join([icon_lookup[category] for category in categories])
-    output['text'] = icon
+    output['text'] = get_icons(categories)
 
     tooltip = []
     for category, progs in categories.items():
         tooltip.append(
-            f'<span color="#8fa1be" font_size="16pt">{category}:</span>')
+            f'\n<span color="#8fa1be" font_size="16pt">{category}</span>')
         for prog in progs:
             tooltip.append(prog)
-    output['tooltip'] = '\n'.join(tooltip)
+    output['tooltip'] = '\n'.join(tooltip).lstrip()
     print(json.dumps(output))
 
 
