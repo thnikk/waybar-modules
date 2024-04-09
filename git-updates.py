1e62511683a8079d2174e12f62ae47ddc70c7b3e
thnikk <thnikkshop@gmail.com>
2024-04-09 14:44:06 -0700

    Added new module for tracking updates to git repo

---
+++ git-updates.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3 -u
+"""
+Description:
+Author:
+"""
+import os
+from subprocess import run, CalledProcessError
+import argparse
+import json
+
+
+class Git:
+    """ d """
+    def __init__(self, path):
+        self.path = path
+
+    def fetch(self) -> None:
+        """ Fetch """
+        try:
+            run(
+                ['git', '-C', self.path, 'fetch'],
+                check=True, capture_output=True
+            )
+        except CalledProcessError as e:
+            print(e.output)
+
+    def commits(self) -> list:
+        """ Pull """
+        return run(
+            [
+                "git", "-C", self.path, "log",
+                "--pretty=format:%h - %s (%cr)", "--abbrev-commit",
+                "--date=relative", "main..origin/main"
+            ],
+            check=True, capture_output=True
+        ).stdout.decode('utf-8').replace('&', 'and').splitlines()
+
+
+def parse_args():
+    """ Parse arguments """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', type=str)
+    return parser.parse_args()
+
+
+def main():
+    """ Main function """
+    args = parse_args()
+    git = Git(os.path.expanduser(args.path))
+    git.fetch()
+    commits = git.commits()
+    if commits:
+        print(json.dumps({
+            "text": f" {len(commits)}",
+            "tooltip": "\n".join(commits)
+        }))
+    else:
+        print(json.dumps({"text": ""}))
+
+
+if __name__ == "__main__":
+    main()
