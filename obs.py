d5dafad447d4d6e6deb9f090b98801553376b5e1
thnikk <thnikkshop@gmail.com>
2024-04-09 11:36:14 -0700

    Hide module if not in use

--- obs.py
+++ obs.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
 """
-Shows the current OBS scene and changes color if recording.
-Use with exec-if: pgrep OBS
+Description: Shows the current OBS scene and changes color if recording.
 Uses obsws-python: https://github.com/aatikturk/obsws-python
+Author: thnikk
 """
 import json
-import sys
 import obsws_python as obs
 
-output = {}
-ICON = ""
 
-try:
-    cl = obs.ReqClient(host='localhost', port=4455, password='password')
-except ConnectionRefusedError:
-    output["text"] = "<span color=\"#d8dee977\">" + ICON + "</span>"
+def main():
+    """ Main function """
+    try:
+        cl = obs.ReqClient(host='localhost', port=4455, password='password')
+        status = cl.get_record_status()
+        scene = cl.get_current_program_scene().current_program_scene_name
+        output = {"text": f" {scene}"}
+        if status.output_duration > 0:
+            output["class"] = 'green'
+            output["tooltip"] = f"{int(status.output_duration/1000)}"
+    except ConnectionRefusedError:
+        output = {"text": ""}
     print(json.dumps(output))
-    sys.exit(0)
 
-status = cl.get_record_status()
-scene = cl.get_current_program_scene().current_program_scene_name
 
-output["text"] = f"{ICON} {scene}"
-if status.output_duration > 0:
-    output["text"] = (f"<span color=\"#bf616a\">{ICON}</span> "
-                      f"{int(status.output_duration/1000)}")
-print(json.dumps(output))
+if __name__ == "__main__":
+    main()
