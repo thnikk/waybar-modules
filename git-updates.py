408f98ea8a5934231269b0439a32ce295c53dd9b
thnikk <thnikkshop@gmail.com>
2024-04-15 15:10:07 -0700

    Tooltip includes changed files

--- git-updates.py
+++ git-updates.py
@@ -7,6 +7,10 @@ import os
 from subprocess import run, CalledProcessError
 import argparse
 import json
+import re
+from datetime import datetime, timezone
+import sys
+from common import print_debug
 
 
 class Git:
@@ -24,16 +28,59 @@ class Git:
         except CalledProcessError as e:
             print(e.output)
 
-    def commits(self) -> list:
-        """ Pull """
-        return run(
-            [
-                "git", "-C", self.path, "log",
-                "--pretty=format:%h - %s (%cr)", "--abbrev-commit",
-                "--date=relative", "main..origin/main"
-            ],
-            check=True, capture_output=True
-        ).stdout.decode('utf-8').replace('&', 'and').splitlines()
+    def commits(self):
+        """ Get commits """
+
+        def plural(num):
+            """ Pluralize word """
+            if num > 1:
+                return 's'
+            return ''
+
+        def get_time(input_list):
+            """ d """
+            for value, word in enumerate(['day', 'hour', 'minute']):
+                if input_list[value]:
+                    return (
+                        f"{input_list[value]} "
+                        f"{word}{plural(input_list[value])} ago")
+            return None
+
+        try:
+            command_output = run(
+                [
+                    'git', '-C', os.path.expanduser(self.path), 'log',
+                    '--name-only', 'main..origin'
+                ],
+                check=True, capture_output=True
+            ).stdout.decode('utf-8')
+        except CalledProcessError as e:
+            lines = e.stderr.decode('utf-8').splitlines()
+            for line in lines:
+                print_debug(line)
+            sys.exit(1)
+        output = {}
+        for line in command_output.splitlines():
+            if re.match('^commit', line):
+                chash = line.split()[1][:7]
+                output[chash] = {
+                    "author": None, "date": None, "msg": None, "files": []}
+            elif re.match('^Author:', line):
+                output[chash]['author'] = \
+                    line.split(":")[1].split("<")[0].strip()
+            elif re.match('^Date:', line):
+                date = ':'.join(line.split(":")[1:]).strip()
+                delta = datetime.now(timezone.utc) - datetime.strptime(
+                    date, '%a %b %d %H:%M:%S %Y %z')
+                dhm = [
+                    delta.days, delta.seconds // 60 // 60, delta.seconds // 60]
+                output[chash]['date'] = get_time(dhm)
+            elif re.match('^ ', line):
+                output[chash]['msg'] = line.strip().replace('&', 'and')
+            else:
+                if line:
+                    output[chash]['files'].append(line)
+        return output
 
 
 def parse_args():
@@ -50,10 +97,20 @@ def main():
     git = Git(os.path.expanduser(args.path))
     git.fetch()
     commits = git.commits()
+
+    tooltip = []
+    for commit, info in commits.items():
+        tooltip.append(
+            f"{commit} - {info['msg']} ({info['date']})"
+        )
+        for file in info['files']:
+            tooltip.append(f'  {file[:20]}')
+        tooltip.append('')
+
     if commits:
         print(json.dumps({
             "text": f"{args.icon} {len(commits)}",
-            "tooltip": "\n".join(commits)
+            "tooltip": "\n".join(tooltip).strip()
         }))
     else:
         print(json.dumps({"text": ""}))
