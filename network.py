8caa4d31cd830c29e9ca277a76009ababb210d5b
thnikk <thnikkshop@gmail.com>
2024-05-08 20:06:45 -0700

    Add argument to only show module when no networks are found

--- network.py
+++ network.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3 -u
 """
-Description:
-Author:
+Description: Network module
+Author: thnikk
 """
 from subprocess import check_output
 import json
+import argparse
+import sys
+
+
+def get_args() -> argparse.ArgumentParser:
+    """ Parse arguments """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-d', action='store_true',
+        help='Only show when no connection is available')
+    return parser.parse_args()
 
 
 def get_devices():
@@ -22,6 +33,7 @@ def get_devices():
 
 def main():
     """ Main function """
+    args = get_args()
     devices = get_devices()
     icons = {"ethernet": "", "wifi": "", "wifi-p2p": ""}
     connection_type = None
@@ -31,16 +43,25 @@ def main():
             connection_type = device['GENERAL.TYPE']
             connection_ip = device['IP4.ADDRESS[1]'].split('/')[0]
 
+    if args.d:
+        if not connection_type:
+            print(json.dumps({"text": " "}))
+        else:
+            print(json.dumps({"text": ""}))
+        sys.exit(0)
+
+
     if connection_type and connection_ip:
         print(json.dumps({
             "text": icons[connection_type],
             "tooltip": f"{connection_type}\n{connection_ip}",
-            # "widget": {"Network": [
-            #     device for device in devices
-            #     if '(connected)' in device['GENERAL.STATE']]}
+            "widget": {"Network": [
+                device for device in devices
+                # if '(connected)' in device['GENERAL.STATE']
+            ]}
         }))
     else:
-        print(json.dumps({"text": ""}))
+        print(json.dumps({"text": " "}))
 
 
 if __name__ == "__main__":
